f040cf54b8a3dacf89d834b189a91d9b57e5175b
boterbaen <isabellesavage.ae@gmail.com>
2024-12-04 16:05:05 -0500

    Switching to EJS rather than using jQuery

--- server.js
+++ server.js
@@ -17,19 +17,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8888;
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.render("index", { output: "<p>Your recipe will appear here.</p>" });
 })
 
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/public/about.html");
+    res.render("about");
 })
 
-app.post('/submit', async (req, res) => {
+app.post('/api', async (req, res) => {
     const data = req.body;
     console.log(data);
     
@@ -39,12 +41,13 @@ app.post('/submit', async (req, res) => {
             { role: "system", content: "You are a code generator." },
             {
                 role: "user",
-                content: "Generate a recipe formatted in HTML as it would appear in the <body> section for the most prominent food item in the movie " + data.movietitle + ", prefaced with a description of the scene that this food item is found in. This response should be on one line (contains no \\n character) and should contain no additional text or characters strictly beyond the HTML content.",
+                content: "Generate a recipe formatted in HTML as it would appear in the <body> section for the most prominent food item in the movie " + data.movietitle + ", prefaced with a description of the scene that this food item is found in. This response should be on one line (contains no \\n character) and should contain no additional text or characters strictly beyond the HTML content. The class of all <div>, <ul>, <li>, and <h1> elements should be \"response\" in order to differentiate the content of this recipe from the rest of the page.",
             },
         ],
     });
+
     console.log(completion.choices[0].message);
-    res.send(completion.choices[0].message.content.replace("`", ""));
+    res.render("index", { output: completion.choices[0].message.content.replace("`", "") });
 });
 
 app.listen(PORT, () => {
